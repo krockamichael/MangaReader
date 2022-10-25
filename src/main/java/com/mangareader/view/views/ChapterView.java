8510538244150a58c22cf898b409adcddc920d4d
krockamichael <krocka.michael@gmail.com>
2022-10-25 19:23:52 +0200

    Create ChapterView
    - create view for displaying manga chapter images
    - currently uses only ReaperScansCrawler

---
+++ src/main/java/com/mangareader/view/views/ChapterView.java
@@ -0,0 +1,35 @@
+package com.mangareader.view.views;
+
+import com.mangareader.crawler.ReaperScansCrawler;
+import com.mangareader.entity.MangaEntity;
+import com.mangareader.view.MyAppLayout;
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.router.PreserveOnRefresh;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+
+@PreserveOnRefresh
+@Route(value = "manga", layout = MyAppLayout.class)
+public class ChapterView extends AbstractVerticalLayout {
+
+  private final transient MangaEntity mangaEntity;
+
+  public ChapterView() {
+    super();
+    mangaEntity = ComponentUtil.getData(UI.getCurrent(), MangaEntity.class);
+    if (mangaEntity != null) {
+      setupImageComponents();
+    }
+  }
+
+  private void setupImageComponents() {
+    ReaperScansCrawler rsCrawler = new ReaperScansCrawler();
+    rsCrawler.parseChapter(mangaEntity.getUrlName())
+        .stream()
+        .map(url -> new Image(new StreamResource(getImageName(url),
+            () -> getClass().getResourceAsStream("/images/" + url)), ""))
+        .forEach(this::add);
+  }
+}
