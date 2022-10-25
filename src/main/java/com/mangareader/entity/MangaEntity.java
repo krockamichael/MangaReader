887728bc9fa7283cc918d7fd3e8e23d2ce54b961
krockamichael <krocka.michael@gmail.com>
2022-10-25 19:28:17 +0200

    Update MangaEntity
    - do not use builder for now as it was incompatible with jackson csv
    - in future create dto and move back to builder (probably)

--- src/main/java/com/mangareader/entity/MangaEntity.java
+++ src/main/java/com/mangareader/entity/MangaEntity.java
@@ -1,19 +1,22 @@
 package com.mangareader.entity;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Manga Entity containing basic information.
  */
 @Getter
-@Builder
+@Setter
+@JsonPropertyOrder({"name", "urlName", "scansName", "scansUrlName", "numOfChapters", "lastReadChapter", "iconPath"})
 public class MangaEntity {
 
-    private String name;
-    private String urlName;
-    private String scansName;
-    private String scansUrlName;
-    private Integer numOfChapters;
-    private Integer lastReadChapter;
+  private String name;
+  private String urlName;
+  private String scansName;
+  private String scansUrlName;
+  private Integer numOfChapters;
+  private Integer lastReadChapter;
+  private String iconPath;
 }
