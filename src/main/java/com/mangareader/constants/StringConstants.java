819d53b8957876ce7b94a7e0f4b6e9cc5abfde24
krockamichael <krocka.michael@gmail.com>
2022-10-29 16:45:04 +0200

    Create class for string constants

---
+++ src/main/java/com/mangareader/constants/StringConstants.java
@@ -0,0 +1,16 @@
+package com.mangareader.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class StringConstants {
+
+  // http
+  public static final String USER_AGENT = "Chrome";
+
+  // style
+  public static final String MARGIN_TOP = "margin-top";
+  public static final String MARGIN_BOTTOM = "margin-bottom";
+  public static final String ZERO = "0";
+}
