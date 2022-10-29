bad4b5b095ad5cfd8f991510c9915203a8bbd582
krockamichael <krocka.michael@gmail.com>
2022-10-29 16:49:44 +0200

    MangaDataProvider
    - use Log4j2
    - use Paths for getting absolute path of directory
    - fix bug where if mangaEntities list was empty data.csv was overwritten with empty values

--- src/main/java/com/mangareader/data/MangaDataProvider.java
+++ src/main/java/com/mangareader/data/MangaDataProvider.java
@@ -6,19 +6,21 @@ import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.mangareader.entity.MangaEntity;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.util.CollectionUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
-@Slf4j
+@Log4j2
 @NoArgsConstructor
 @SuppressWarnings("java:S1075") // CSV_FILE_PATH as customizable parameter
 public class MangaDataProvider {
 
-  private static final String CSV_FILE_PATH = "C:\\Users\\krock\\Desktop\\mangareader\\src\\main\\resources\\static\\data.csv";
+  private static final String CSV_FILE_PATH = Paths.get("src/main/resources/static/data.csv").toAbsolutePath().toString();
   private static final List<MangaEntity> mangaEntities = readMangaEntitiesFromCSV();
 
   private static List<MangaEntity> readMangaEntitiesFromCSV() {
@@ -37,12 +39,20 @@ public class MangaDataProvider {
         .readValues(csvFile)) {
       return mangasIter.readAll();
     } catch (IOException e) {
-      log.error(e.getMessage());
+      log.error(e);
     }
     return Collections.emptyList();
   }
 
   public static void onExit() {
+    writeToCsv();
+  }
+
+  private static void writeToCsv() {
+    if (CollectionUtils.isEmpty(mangaEntities)) {
+      return;
+    }
+
     File csvOutputFile = new File(CSV_FILE_PATH);
     CsvMapper csvMapper = new CsvMapper();
 
@@ -56,7 +66,7 @@ public class MangaDataProvider {
     try {
       writer.writeValues(csvOutputFile).writeAll(mangaEntities);
     } catch (IOException e) {
-      log.error(e.getMessage());
+      log.error(e);
     }
   }
 
