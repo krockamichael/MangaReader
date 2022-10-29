ec887fc03ed87f29d12c62cb46ff88f9af7f0b5e
krockamichael <krocka.michael@gmail.com>
2022-10-29 13:50:42 +0200

    Update pom.xml and application.properties
    - atmosphere log level set to warn
    - pom.xml:
    * add repositories (maven, vaadin-addons)
    * add dependency - on startup launch app in browser
    * add dependency - java live reload

--- src/main/java/com/mangareader/MangareaderApplication.java
+++ src/main/java/com/mangareader/MangareaderApplication.java
@@ -3,14 +3,17 @@ package com.mangareader;
 import com.mangareader.data.MangaDataProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 import javax.annotation.PreDestroy;
 
+@EnableAsync
 @SpringBootApplication
 public class MangareaderApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MangareaderApplication.class, args);
+    LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(MangareaderApplication.class, args));
   }
 
   @PreDestroy
