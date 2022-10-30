ac0983ebc4d47eeb0fa4be41f56c3b7b6214cb5a
krockamichael <krocka.michael@gmail.com>
2022-10-30 12:32:51 +0100

    Remove logic for loading and saving of files
    - remove all async logic for loading and saving files

--- src/main/java/com/mangareader/service/crawler/AbstractCrawler.java
+++ src/main/java/com/mangareader/service/crawler/AbstractCrawler.java
@@ -1,126 +1,12 @@
 package com.mangareader.service.crawler;
 
-import com.vaadin.flow.internal.Pair;
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.time.StopWatch;
-import org.springframework.scheduling.annotation.Async;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
-import static com.mangareader.constants.StringConstants.TARGET_DOWNLOAD_DIR;
-import static com.mangareader.constants.StringConstants.USER_AGENT;
 
 @Log4j2
 public abstract class AbstractCrawler {
-
-  static final StopWatch STOP_WATCH = new StopWatch();
-  protected final Set<String> filenames = new LinkedHashSet<>();
-  private final ExecutorService executor = Executors.newFixedThreadPool(8); // newCachedThreadPool
-
-  public static BufferedImage resize(BufferedImage img, int newW, int newH) {
-    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
-    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
-
-    Graphics2D g2d = dimg.createGraphics();
-    g2d.drawImage(tmp, 0, 0, null);
-    g2d.dispose();
-
-    return dimg;
-  }
-
-  void stopStopWatch() {
-    STOP_WATCH.stop();
-    float timeSpent = STOP_WATCH.getTime() / 1000.f;
-    if (timeSpent != 0.f) {
-      log.info(String.format("Downloading images took %.2f seconds.", timeSpent));
-    }
-    STOP_WATCH.reset();
-  }
-
-  @Async
-  Future<BufferedImage> asyncLoadImageContent(String imageUrl) {
-    return executor.submit(() -> getImage(imageUrl));
-  }
-
-  @Async
-  Future<Boolean> asyncWriteImage(String fileName, BufferedImage image) {
-    return executor.submit(() -> ImageIO.write(image, "png", new File(fileName)));
-  }
-
-  private BufferedImage getImage(String imageUrl) {
-    BufferedImage image = null;
-    try {
-      URL url = new URL(imageUrl);
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestProperty("User-Agent", USER_AGENT);
-      image = ImageIO.read(connection.getInputStream());
-    } catch (IOException e) {
-      log.error(e);
-    }
-    return image;
-  }
 
   protected String toUrl(String baseUrl, String input) {
     return baseUrl.concat(input);
   }
 
-  Pair<String, String> toPngFilename(String mangaName, String chapterNumber, String filename) {
-    String fixedFilename = "";
-
-    if (filename.contains("/")) {
-      String[] splitFilename = filename.split("/");
-      fixedFilename = splitFilename[splitFilename.length - 1];
-    }
-    String targetDir = mangaName
-        .concat("\\")
-        .concat(chapterNumber)
-        .concat("\\");
-    fixedFilename = targetDir.concat(fixedFilename.replace(".jpg", ".png"));
-    filenames.add(fixedFilename);
-
-    // TODO: refactor to call only once
-    createDirIfNotExist(targetDir);
-
-    return new Pair<>(fixedFilename, TARGET_DOWNLOAD_DIR.concat(fixedFilename));
-  }
-
-  Pair<String, String> toPngFilename(String mangaName, String filename) {
-    String fixedFilename = "";
-
-    if (filename.contains("/")) {
-      String[] splitFilename = filename.split("/");
-      fixedFilename = splitFilename[splitFilename.length - 1];
-    }
-    String targetDir = mangaName.concat("\\icon\\");
-    fixedFilename = targetDir.concat(fixedFilename.replace(".jpg", ".png"));
-
-    createDirIfNotExist(targetDir);
-
-    return new Pair<>(fixedFilename, TARGET_DOWNLOAD_DIR.concat(fixedFilename));
-  }
-
-  private void createDirIfNotExist(String targetDir) {
-    File mangaDir = new File(TARGET_DOWNLOAD_DIR.concat(targetDir));
-    if (!mangaDir.exists()) {
-      boolean result = mangaDir.mkdirs();
-      if (!result) {
-        log.error("The creation of {} directory failed.", targetDir);
-      }
-    }
-  }
-
-  void clearFileNames() {
-    filenames.clear();
-  }
 }
--- src/main/java/com/mangareader/service/crawler/ReaperScansCrawler.java
+++ src/main/java/com/mangareader/service/crawler/ReaperScansCrawler.java
@@ -1,9 +1,8 @@
 package com.mangareader.service.crawler;
 
 import com.mangareader.entity.MangaEntity;
-import com.vaadin.flow.internal.Pair;
+import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.commons.lang3.BooleanUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,24 +11,18 @@ import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.Collections;
+import java.util.List;
 
 import static com.mangareader.constants.StringConstants.USER_AGENT;
 
 @Log4j2
 @Service
+@NoArgsConstructor
 public class ReaperScansCrawler extends AbstractCrawler {
 
   private static final String BASE_URL = "https://reaperscans.com/comics/";
-
-  public ReaperScansCrawler() {
-    clearFileNames();
-  }
 
   public List<String> parseChapter(MangaEntity entity) {
     try {
@@ -79,102 +72,6 @@ public class ReaperScansCrawler extends AbstractCrawler {
         .stream()
         .map(e -> e.attr("src"))
         .toList();
-  }
-
-  private void loadAndSaveImages(String mangaName, String numOfChapters, List<String> imageUrls) {
-    Map<Pair<String, String>, Future<BufferedImage>> futureMap = new HashMap<>();
-
-    for (String imageUrl : imageUrls) {
-      Pair<String, String> fileNamePair = toPngFilename(mangaName, numOfChapters, imageUrl);
-      if (fileExists(fileNamePair.getSecond())) {
-        continue;
-      }
-
-      futureMap.put(fileNamePair, asyncLoadImageContent(imageUrl));
-    }
-    getAsyncResponses(futureMap);
-  }
-
-  private Future<Boolean> writeImage(Pair<String, String> fileNamePair, Future<BufferedImage> future, boolean isIcon) {
-    BufferedImage image;
-    try {
-      image = future.get();
-    } catch (InterruptedException | ExecutionException e) {
-      log.error(e);
-      return null;
-    }
-    if (isIcon) {
-      image = resize(image, 100, 150);
-    }
-
-    filenames.add(fileNamePair.getFirst());
-    return asyncWriteImage(fileNamePair.getSecond(), image);
-  }
-
-  private void getAsyncResponses(Map<Pair<String, String>, Future<BufferedImage>> futureMap) {
-    STOP_WATCH.start();
-
-    while (futureMap.size() != 0) {
-      Iterator<Map.Entry<Pair<String, String>, Future<BufferedImage>>> it = futureMap.entrySet().iterator();
-      while (it.hasNext()) {
-        Map.Entry<Pair<String, String>, Future<BufferedImage>> entry = it.next();
-        if (entry.getValue().isDone()) {
-          writeImage(entry.getKey(), entry.getValue(), false);
-          log.info("{} is finished.", entry.getKey().getFirst());
-          it.remove();
-        }
-      }
-    }
-    stopStopWatch();
-  }
-
-  private boolean fileExists(String path) {
-    return new File(path).exists();
-  }
-
-  public void parseAndSaveIcon(MangaEntity entity) {
-    try {
-      Document document = Jsoup.connect(toUrl(BASE_URL, entity.getUrlName()))
-          .userAgent(USER_AGENT)
-          .get();
-
-      String iconUrl = document.select("div > img[src]")
-          .stream()
-          .map(e -> e.attr("src"))
-          .findFirst()
-          .orElse(null);
-
-      // TODO: move to get png name method ??????????
-      if (iconUrl == null) {
-        return;
-      }
-
-      Pair<String, String> fileNamePair = toPngFilename(entity.getUrlName(), iconUrl);
-      if (fileExists(fileNamePair.getSecond())) {
-        entity.setIconPath(fileNamePair.getFirst()); // FIXME - should not be needed here
-        return;
-      }
-
-      Future<BufferedImage> bufferedImageFuture = asyncLoadImageContent(iconUrl);
-      while (!bufferedImageFuture.isDone()) {
-      }
-      if (bufferedImageFuture.isDone()) {
-        Future<Boolean> z = writeImage(fileNamePair, bufferedImageFuture, true);
-        try {
-          assert z != null;
-          Boolean bla = z.get();
-          if (BooleanUtils.isTrue(bla) && z.isDone()) {
-            entity.setIconPath(fileNamePair.getFirst());
-          }
-        } catch (InterruptedException | ExecutionException e) {
-          log.error(e);
-        }
-      }
-      // TODO: save icon
-      //  set url to entity
-    } catch (IOException e) {
-      log.error(e);
-    }
   }
 
   @Async
